b750262baeacad5b1918361c2cec9f66d120554e
Bohdan Zhuvak <zhuvak.bohdan@gmail.com>
2025-01-30 21:11:17 +0100

    fix: fix context loading in test, remove .idea from git
    Fixed the issue where the application context failed to load in tests due to a missing MongoDB instance.

--- order-service/src/test/java/com/bohdanzhuvak/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/bohdanzhuvak/orderservice/OrderServiceApplicationTests.java
@@ -2,13 +2,27 @@ package com.bohdanzhuvak.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class OrderServiceApplicationTests {
 
-  //@Test
-  //void contextLoads() {
-  //}
+  @Container
+  static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0");
 
+  @DynamicPropertySource
+  static void setProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+  }
+
+  @Test
+  void contextLoads() {
+    //Test context loading with Mongo test container
+  }
 }
 
