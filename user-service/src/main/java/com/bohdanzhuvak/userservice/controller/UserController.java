8f40d48b9d4d2db4db411e819481ab175a07d42f
Bohdan Zhuvak <zhuvak.bohdan@gmail.com>
2025-03-04 17:51:01 +0100

    feat(user-service): add getAllUsers, deleteUser methods

--- user-service/src/main/java/com/bohdanzhuvak/userservice/controller/UserController.java
+++ user-service/src/main/java/com/bohdanzhuvak/userservice/controller/UserController.java
@@ -3,11 +3,14 @@ package com.bohdanzhuvak.userservice.controller;
 import com.bohdanzhuvak.userservice.dto.UserResponse;
 import com.bohdanzhuvak.userservice.service.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,19 @@ public class UserController {
   @PreAuthorize("hasRole('USER')")
   public UserResponse getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
     return userService.getUserById(userDetails.getUsername());
+  }
+
+  @GetMapping
+  @SecurityRequirement(name = "Bearer Authentication")
+  @PreAuthorize("hasRole('ADMIN')")
+  public List<UserResponse> getAllUsers() {
+    return userService.getAllUsers();
+  }
+
+  @DeleteMapping("{id}")
+  @SecurityRequirement(name = "Bearer Authentication")
+  @PreAuthorize("hasRole('USER')")
+  public void deleteUser(@PathVariable("id") String id) {
+    userService.deleteUserById(id);
   }
 }
--- user-service/src/main/java/com/bohdanzhuvak/userservice/service/UserService.java
+++ user-service/src/main/java/com/bohdanzhuvak/userservice/service/UserService.java
@@ -4,6 +4,8 @@ import com.bohdanzhuvak.commonexceptions.exception.impl.ResourceNotFoundExceptio
 import com.bohdanzhuvak.userservice.dto.UserResponse;
 import com.bohdanzhuvak.userservice.model.User;
 import com.bohdanzhuvak.userservice.repository.UserRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,18 @@ public class UserService {
   public UserResponse getUserById(String id) {
     return userRepository.findById(id).map(this::mapToResponse)
         .orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found"));
+  }
+
+  public List<UserResponse> getAllUsers() {
+    return userRepository.findAll().stream().map(this::mapToResponse).collect(Collectors.toList());
+  }
+
+  public void deleteUserById(String id) {
+    userRepository.findById(id)
+        .ifPresentOrElse(
+            userRepository::delete,
+            () -> {throw new ResourceNotFoundException("User with id " + id + " not found");}
+        );
   }
 
   private UserResponse mapToResponse(User user) {
